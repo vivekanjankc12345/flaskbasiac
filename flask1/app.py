163a596dba8029365963169a17fb7b9ea155ec3a
Vivek Anjan <vivekanjankc12345@gmail.com>
2023-07-03 19:17:20 +0530

    gggh

--- flask1/app.py
+++ flask1/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Welcome to the Flask application!'
+    return 'Welcome!'
 
 @app.route('/greet/<username>')
 def greet_user(username):
